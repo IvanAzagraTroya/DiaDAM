4b66e0349c91d37a6409b179834755d00f63fed1
Iván Azagra Troya <67174666+IvanAzagraTroya@users.noreply.github.com>
2022-04-28 12:23:30 +0200

    Add files via upload

---
+++ DiaDAM/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module es.diadam.diadam {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens es.diadam.diadam to javafx.fxml;
+    exports es.diadam.diadam;
+}
