22ebc85a208ac229eb57349621615dcd5ec1f18d
Jorge Sanchez <pejor3212@gmail.com>
2022-05-18 09:42:44 +0200

    interfaz controlador

--- DiaDAM/src/main/java/module-info.java
+++ DiaDAM/src/main/java/module-info.java
@@ -1,14 +1,12 @@
 module es.diadam.diadam {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
+
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires java.sql;
     requires org.mybatis;
     requires lombok;
