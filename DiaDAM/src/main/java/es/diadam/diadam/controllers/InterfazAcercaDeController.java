981a3d943335f7ff02cdcbab9f81bfe1a1877ed4
veronicanchitipan <92951273+veronicanchitipan@users.noreply.github.com>
2022-05-20 09:54:55 +0200

    Interfaz acerca, splash con sus respectivos controladores.

--- DiaDAM/src/main/java/es/diadam/diadam/controllers/InterfazAcercaDeController.java
+++ DiaDAM/src/main/java/es/diadam/diadam/controllers/InterfazAcercaDeController.java
@@ -1,10 +1,42 @@
 package es.diadam.diadam.controllers;
 
 
+import es.diadam.diadam.utils.Properties;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * @author Veronica Anchitipan
  */
 
 public class InterfazAcercaDeController {
-    // comentario para push
+    //push comentario
+    Logger logger = LogManager.getLogger(InterfazAcercaDeController.class);
+    // complementos equipo
+    private Stage dialogStage;
+
+    @FXML
+    private Label nombres;
+    @FXML
+    private Label repositorio;
+    @FXML
+    private Label version;
+    @FXML
+    private Hyperlink githubLink;
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+    @FXML
+    private void initialize() {
+
+        version.setText(Properties.APP_VERSION);
+        nombres.setText(Properties.APP_AUTHORS);
+        githubLink.setText(Properties.APP_TITLE);
+        repositorio.setText(Properties.APP_TITLE);
+    }
 }
--- DiaDAM/src/main/java/es/diadam/diadam/controllers/InterfazSplashController.java
+++ DiaDAM/src/main/java/es/diadam/diadam/controllers/InterfazSplashController.java
@@ -1,4 +1,68 @@
 package es.diadam.diadam.controllers;
 
-public class InterfazSplashController {
+import es.diadam.diadam.managers.SceneManager;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Veronica Anchitipan
+ */
+
+public class InterfazSplashController implements Initializable {
+    Logger logger = LogManager.getLogger(InterfazSplashController.class);
+    @FXML
+    private ImageView fondo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // imagen-fondo-animación
+        FadeTransition transition = new FadeTransition(Duration.millis(3000), fondo);
+        transition.setFromValue(1.0);
+        transition.setToValue(1.0);
+        transition.play();
+
+        transition.setOnFinished(event -> {
+            // cierre
+            Stage ventana = (Stage) fondo.getScene().getWindow();
+            ventana.hide();
+            // Visualización ventana principal
+            SceneManager sceneManager = SceneManager.get();
+            try {
+                sceneManager.initSplash(ventana);
+            } catch (IOException | InterruptedException e) {
+                logger.error(e.getMessage());
+                e.printStackTrace();
+            }
+
+
+        });
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
